cde2cfc443cdf8c148d19863b39e335f153b4d9e
Jonatan Schultz <jonatan@jschultz.se>
2025-03-14 09:12:53 +0100

    boilerplate

---
+++ sesamy2.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://sesamy.com
+ * @since             1.0.0
+ * @package           Sesamy2
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Sesamy2
+ * Plugin URI:        https://sesamy.com
+ * Description:       This is a description of the plugin.
+ * Version:           1.0.0
+ * Author:            Sesamy
+ * Author URI:        https://sesamy.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       sesamy2
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'SESAMY2_VERSION', '1.0.0' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-sesamy2.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_sesamy2() {
+
+	$plugin = new Sesamy2();
+	$plugin->run();
+
+}
+run_sesamy2();
